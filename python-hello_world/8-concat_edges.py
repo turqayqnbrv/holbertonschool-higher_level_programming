7076e1b6f282368408b7da543197bfadf93136d8
turqayqnbrv <turqayedu@gmail.com>
2026-03-30 18:29:33 +0000

    task8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-words = str.split()
-str = " ".join(words[5:7]) + " with " + words[0]
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = " ".join(str.split()[5:7]) + " with " + str.split()[0]
 print(str)
