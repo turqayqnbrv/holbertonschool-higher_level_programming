c51e6b1fb2b432a1f54aab3f47838026cd9f1184
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-04-13 17:45:47 +0400

    Fix docstring formatting in BaseGeometry class

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry"""
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """Represent base geometry"""
+    """Represent base geometry."""
 
     def area(self):
-        """Not implemented"""
+        """Not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate a parameter as an integer
+        """Validate a parameter as an integer.
+
         Args:
-            name (str): The name of the parameter
-            value (int): The parameter to validate
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
         Raises:
-            TypeError: If value is not an integer
-            ValueError: If value is <= 0
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
