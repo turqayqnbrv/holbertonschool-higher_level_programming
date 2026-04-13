c58519adb9865966ed98b6a4fee6f02bd214475b
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-04-13 17:40:50 +0400

    Update docstrings in BaseGeometry class

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""Module that contains the BaseGeometry class"""
+"""Defines a base geometry class BaseGeometry"""
 
 
 class BaseGeometry:
-    """A class with area and integer_validator methods"""
+    """Class representing base geometry"""
 
     def area(self):
-        """Raises an Exception with the message area() is not implemented"""
+        """Raises an Exception because area is not implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates value"""
+        """Validates that value is a positive integer
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
