61bbf3e3841f77d52a24c409d100a4f4de51cdbf
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-04-13 16:57:41 +0400

    task3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+"""Defines a Square class."""
+
+
 class Square:
-    """Defines a square"""
+    """A class that defines a square with area method."""
+
     def __init__(self, size=0):
-        """Initialises the data"""
-        self.__size = size
-        if type(size) != int:
+        """Initialize with optional size."""
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
-        """Returns current square area"""
-        return self.__size**2
+        """Return the current square area."""
+        return self.__size ** 2
